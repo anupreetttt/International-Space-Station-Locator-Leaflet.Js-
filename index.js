efdf1a3b457256f7463ebbef9561b2e21d3d9e2a
Anupreet Paulkar <anupreet@Anupreets-MacBook-Pro.local>
2023-07-27 02:35:08 -0500

    Integrated OpenStreetMap using Leaflet.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap'
+}).addTo(mymap);
+
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 async function getData() {
