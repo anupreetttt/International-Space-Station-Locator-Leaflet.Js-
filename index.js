e3913f9f40b2d12cdf9fd434b853f5f8d2a97c3e
Anupreet Paulkar <anupreet@Anupreets-MacBook-Pro.local>
2023-07-27 03:42:42 -0500

    Marker and radius added

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+const mymap = L.map('issMap').setView([51.505, -0.09], 5);
+const marker = L.marker([0,0]).addTo(mymap);
+
+const circle = L.circle([0, 0], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 5000
+}).addTo(mymap);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap'
 }).addTo(mymap);
 
+
+
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
+let firstZoom = true;
 async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
 
-    console.log(data.latitude);
 
-    document.getElementById('lat').textContent = data.latitude;
+    const longitude = data.longitude;
+    const latitude = data.latitude;
 
-    document.getElementById('long').textContent = data.longitude;
+    console.log(longitude);
+
+    marker.setLatLng([latitude, longitude]);
+
+    circle.setLatLng([latitude, longitude]);
+    if(firstZoom) {
+    mymap.setView([latitude, longitude], 2)
+    firstZoom = false;    
+}
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+
+    document.getElementById('long').textContent = longitude.toFixed(2);
 
 }
 
 getData();
+
+setInterval(getData, 1000); 
